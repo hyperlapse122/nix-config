12e793293f1975285d135a49b35b75aa50657486
Joosung Park <iam@h82.dev>
2026-05-03 14:27:22 +0900

    chore(1password): configure agents to include ssh keys in multiple vaults

--- home/modules/_1password.nix
+++ home/modules/_1password.nix
@@ -29,6 +29,44 @@ in
     #       do browser integration / system keyring / SSH agent work properly. Pinning a Nix-store
     #       path or /opt path bypasses the wrapper and breaks integration.
     #       (Same comment as the autostart .desktop in hosts/common/programs/_1password.nix)
+    xdg.configFile."1Password/ssh/agent.toml".text = ''
+      # This is the 1Password SSH agent config file, which allows you to customize the
+      # behavior of the SSH agent running on this machine.
+      #
+      # You can use it to:
+      # * Enable keys from other vaults than the Private vault
+      # * Control the order in which keys are offered to SSH servers
+      #
+      # EXAMPLE
+      #
+      # By default, all keys in your Private vault(s) are enabled:
+      #
+      #  [[ssh-keys]]
+      #  vault = "Private"
+      #
+      # You can enable more keys by adding more `[[ssh-keys]]` entries.
+      # For example, to first enable item "My SSH Key" from "My Custom Vault":
+      #
+      #  [[ssh-keys]]
+      #  item = "My SSH Key"
+      #  vault = "My Custom Vault"
+      #
+      #  [[ssh-keys]]
+      #  vault = "Private"
+      #
+      # You can test the result by running:
+      #
+      #  SSH_AUTH_SOCK=~/.1password/agent.sock ssh-add -l
+      #
+      # More examples can be found here:
+      #  https://developer.1password.com/docs/ssh/agent/config
+      [[ssh-keys]]
+      item = "cwgsuvmyhwtnwiir3sqcdeaglm"
+
+      [[ssh-keys]]
+      item = "5rocflo3i7h7yyich4e4k4zqgi"
+    '';
+
     xdg.dataFile."applications/1password.desktop".text = ''
       [Desktop Entry]
       Name=1Password
